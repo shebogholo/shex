bba910977ec0951c3de2fa211fe57b8fef7e9687
Shebogholo <shebogholo@gmail.com>
2023-01-13 11:45:41 +0300

    added install script

--- install.sh
+++ install.sh
@@ -1,7 +1,39 @@
 #!/bin/sh
 
-# wget file from github link
-wget https://github.com/shebogholo/shex/releases/download/v0.0.1/shex-amd64-darwin -O shex
+uname_os() {
+  os=$(uname -s | tr '[:upper:]' '[:lower:]')
+  echo "$os"
+}
 
-## copy file to executable path
-cp shex /usr/local/bin/
+
+uname_arch() {
+  arch=$(uname -m)
+  case $arch in
+    x86_64) arch="amd64" ;;
+    x86) arch="386" ;;
+    i686) arch="386" ;;
+    i386) arch="386" ;;
+    aarch64) arch="arm64" ;;
+    armv*) arch="armv6" ;;
+    armv*) arch="armv6" ;;
+    armv*) arch="armv6" ;;
+  esac
+  echo ${arch}
+}
+
+TAG="v0.0.1"
+OS=$(uname_os)
+ARCH=$(uname_arch)
+INSTALL_DIR="/usr/local/bin/"
+
+BINARY_URL="https://github.com/shebogholo/shex/releases/download/$TAG/shex-$ARCH-$OS"
+
+tmp=$(mktemp -d)
+
+binary=$tmp/shex
+
+$(curl $BINARY_URL -sL -o $binary)
+
+sudo install "$binary" $INSTALL_DIR
+
+rm -rf "$tmp"
