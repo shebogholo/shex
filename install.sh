8af0cc2592960a9a3d5c5c38b1604dab8c7900a0
Shebogholo <shebogholo@gmail.com>
2023-01-13 11:17:24 +0300

    added script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# wget file from github link
+wget https://github.com/shebogholo/shex/releases/download/v0.0.1/shex-amd64-darwin -O shex
+
+## copy file to executable path
+cp shex /usr/local/bin/
